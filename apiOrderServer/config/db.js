f507d53172b299fb055edd1663d5e4375c031e38
นที สังข์ทองงาม <natee.sun@gmail.com>
2020-04-06 21:02:33 +0700

    update config files

--- apiOrderServer/config/db.js
+++ apiOrderServer/config/db.js
@@ -7,16 +7,4 @@ const connection = mysql.createPool({
   database: "pos_takeorder"
 })
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "mysql5password",
-//   database: "pos_takeorder"
-// })
-
-// connection.connect(function(err) {
-//   if (err) throw err
-//   console.log("Connected!")
-// })
-
 module.exports = connection
