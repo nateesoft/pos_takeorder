4e1edb2b1d7fccd1808debd8c8d59e735230c821
นที สังข์ทองงาม <natee.sun@gmail.com>
2020-08-03 22:30:25 +0700

    add template file import

---
+++ web-order/app/test/test_read_excel.js
@@ -0,0 +1,19 @@
+import * as XLSX from "xlsx"
+
+//f = file
+var name = './Import_Template.xlsx'
+const reader = new FileReader()
+reader.onload = (evt) => {
+  // evt = on_file_select event
+  /* Parse data */
+  const bstr = evt.target.result
+  const wb = XLSX.read(bstr, { type: "binary" })
+  /* Get first worksheet */
+  const wsname = wb.SheetNames[0]
+  const ws = wb.Sheets[wsname]
+  /* Convert array of arrays */
+  const data = XLSX.utils.sheet_to_csv(ws, { header: 1 })
+  /* Update state */
+  console.log("Data>>>" + data)
+}
+reader.readAsBinaryString(f)
